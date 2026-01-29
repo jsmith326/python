82b46f645f7e430bd0ba6be05371f10a8b0382a6
jsmith326 <jsmith326@students.mchenry.edu>
2026-01-29 10:27:21 -0600

    Initial upload

---
+++ joke.py
@@ -0,0 +1,2 @@
+print("A woman gets off a plane with a 6 foot 5 Zulu warrior at the airport")
+print("the womans mother is waiting for her and him and upon seeing the warrior exclaims I said a rich doctor!")
